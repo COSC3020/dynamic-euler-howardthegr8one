b022d34fb6971d84c57d340be14004b7014a8bdd
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-04-09 13:49:38 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,8 @@
-function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+// tail-recursive factorial function is never used but still tested
+function factorial(n, iters = 0, accum = 1) {
+    return (n === iters) ? accum : factorial(n, iters+=1, accum *= iters)
 }
 
-function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+function e(n, sum = 1, iters = 1, fact = 1) {
+    return (n === 0) ? sum : e(n-1, sum += (1.0 / fact), iters+=1, fact*=iters)
 }
